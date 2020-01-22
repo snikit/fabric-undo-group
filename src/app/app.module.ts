468255772a9ba00e81925a1526314cfd9583e469
snikit <snikitz@yahoo.com>
2020-01-22 12:30:22 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { UndoService } from './undo/undo.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [UndoService]
 })
 export class AppModule {}
---
+++ src/app/undo/undo.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UndoService {
+
+  constructor() { }
+
+
+  save(){
+
+
+
+  }
+
+
+  undo(){
+
+
+  }
+
+
+  
+
+}
